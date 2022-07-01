0bfa86b679ba7e390eec1781ce29da9f03cf580d
Dyvine57 <dulana57@gmail.com>
2022-07-01 00:56:48 -0400

    cleanup

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -1,27 +1,20 @@
-﻿using HtmlAgilityPack;
-using Microsoft.Win32;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System;
 using System.IO;
 using System.Linq;
 using System.Media;
-using System.Net.Cache;
+using System.Diagnostics;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shell;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using HtmlAgilityPack;
 
 namespace YouTubeStuff {
     /// <summary>
